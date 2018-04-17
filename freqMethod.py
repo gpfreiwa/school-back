5b9d6fdef23eb15d3013e4b1dc8520645a30d770
gpfreiwa <gpfreiwa@asu.edu>
2018-04-16 17:09:00 -0700

    Made change to freqMethod file

--- freqMethod.py
+++ freqMethod.py
@@ -4,13 +4,15 @@ m = [0,1,2,3,4]
 n = [0,1,2,3,4]
 p = [0,1,2,3,4]
 
-a = 0.08
-b = 0.06
-d = 0.05
-f = []
+if __name__ == '__main__':
 
-for i in m:
-    for j in n:
-        for k in p:
-            f[int(i),int(j),int(k)] = (c/2)*sqrt((i/a)*(i/a)+(j/b)*(j/b)+(k/d)*(k/d))
+    a = 0.08
+    b = 0.06
+    d = 0.05
+    f = []
+
+    for i in m:
+        for j in n:
+            for k in p:
+                f[int(i),int(j),int(k)] = (c/2)*sqrt((i/a)*(i/a)+(j/b)*(j/b)+(k/d)*(k/d))
 
