699c02df07d807c52e5e987c6d0e7ede5d7d38a7
gpfreiwa <gpfreiwa@asu.edu>
2018-04-16 20:10:35 -0700

    updated the file for more ability as a function

--- freqMethod.py
+++ freqMethod.py
@@ -1,18 +1,19 @@
 from math import sqrt
 c = 3 * 10^8
-m = [0,1,2,3,4]
-n = [0,1,2,3,4]
-p = [0,1,2,3,4]
 
-if __name__ == '__main__':
-
-    a = 0.08
-    b = 0.06
-    d = 0.05
+def freqMethod(a,b,c):
     f = []
+    m = [0,1,2,3,4]
+    n = [0,1,2,3,4]
+    p = [0,1,2,3,4]
 
     for i in m:
         for j in n:
             for k in p:
                 f[int(i),int(j),int(k)] = (c/2)*sqrt((i/a)*(i/a)+(j/b)*(j/b)+(k/d)*(k/d))
 
+if __name__ == '__main__':
+    a = 0.08
+    b = 0.06
+    d = 0.05
+    freqMethod(a,b,d)
